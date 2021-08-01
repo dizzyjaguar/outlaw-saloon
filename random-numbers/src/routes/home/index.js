0226787683279ba54728a6a7529ff2c1cf054f17
Eli Zevin <dizzyjaguar@gmail.com>
2021-07-31 18:22:57 -0700

    cleaning and creating some scaffolding in preact

--- random-numbers/src/routes/home/index.js
+++ random-numbers/src/routes/home/index.js
@@ -1,11 +1,17 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import style from './style.css';
 
-const Home = () => (
+const Home = () => {
+	// this should eventually generate a certain amount of random numbers between 2 given numbers, list them out and then maybe somehow export them to be easily applied to a csv file 
+	const [numberList, setNumberList] = useState([])
+
+	return (
 	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
+		<h1>hi</h1>
+		<p>{ numberList }</p>
 	</div>
-);
+	)
+};
 
 export default Home;
---
+++ random-numbers/src/utils/numberGenerator.js
@@ -0,0 +1,1 @@
+// this is where my code for the functions will go
