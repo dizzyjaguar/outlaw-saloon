72ebcc7d42cfcb577c633bcfcac311f0e1fd4390
Eli Zevin <dizzyjaguar@gmail.com>
2021-08-01 12:57:16 -0700

    install linkstate to make easier form work

--- random-numbers/src/components/NumInput/index.js
+++ random-numbers/src/components/NumInput/index.js
@@ -1,12 +1,21 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 
 const NumInput = () => {
 
+  const [values, setValues] = useState({
+    howMany: 0,
+    lowNum: 0,
+    highNum: 0
+  });
+
   const onChange = (event) => {
     const { target } = event;
-    const { value } = target;
-    
-  }
+    const { name, value } = target;
+    event.linkstate()
+
+    setValues({ ...values, [name]: value });
+  };
   
 
   return (
@@ -15,15 +24,15 @@ const NumInput = () => {
     <form>
       <label>
         How Many
-      <input type='number' onInput={onChange} />
+      <input type='number' value={values.howMany} onInput={onChange} />
       </label>
       <label>
         Low Number
-      <input type='number' onInput={onChange} />
+      <input type='number' value={values.lowNum} onInput={onChange} />
       </label>
       <label>
         High Number
-      <input type='number' onInput={onChange} />
+      <input type='number' value={values.highNum} onInput={onChange} />
       </label>
     </form>
     </>
