3fb8714d2c622d99e4036a6c83698b0201546ac8
Eli Zevin <dizzyjaguar@gmail.com>
2021-08-02 10:49:33 -0700

    css on list

--- random-numbers/src/components/NumList/index.js
+++ random-numbers/src/components/NumList/index.js
@@ -1,17 +1,18 @@
 import { h } from 'preact';
+import style from './style.css';
 
 const NumList = ({numberList}) => {
   
   return (
-    <>
-      <ul>
+    <div class={style.div}>
+      <ul class={style.ul}>
 			  {
 			  numberList.map(num => {
 				  return <li>{num}</li>
 			  })
 		    }
 		  </ul>
-    </>
+    </div>
   )
 }
 
